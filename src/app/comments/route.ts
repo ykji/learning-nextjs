03568183b1c387d30064250cafac788ec77a8968
ykji <yvarshney44@gmail.com>
2024-01-11 22:58:35 +0530

    GET and POST

---
+++ src/app/comments/route.ts
@@ -0,0 +1,22 @@
+import { comments } from "./data";
+
+export async function GET() {
+  return Response.json(comments);
+}
+
+export async function POST(request: Request) {
+  const comment = await request.json();
+
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    status: 201,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
