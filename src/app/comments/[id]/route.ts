79f3d330079bd5f6cb64d24f30bee275ddd15f15
ykji <yvarshney44@gmail.com>
2024-01-11 23:05:36 +0530

    dynamic get request

---
+++ src/app/comments/[id]/route.ts
@@ -0,0 +1,9 @@
+import { comments } from "../data";
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  const { id } = params;
+
+  const comment = comments.find((comment) => comment.id === parseInt(id));
+
+  return Response.json(comment);
+}
