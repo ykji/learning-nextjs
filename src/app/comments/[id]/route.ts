12c5d68df1e936aae296754e62034fabe4ab6f8c
ykji <yvarshney44@gmail.com>
2024-01-17 18:05:05 +0530

    redirect in case of invalid

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,7 +1,12 @@
 import { comments } from "../data";
+import { redirect } from "next/navigation";
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
   const { id } = params;
+
+  if (parseInt(id) > comments.length) {
+    redirect("/comments");
+  }
 
   const comment = comments.find((comment) => comment.id === parseInt(id));
 
