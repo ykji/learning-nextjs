b9691775562073d805eb4979d290e43aaacfc870
ykji <yvarshney44@gmail.com>
2024-01-17 18:01:15 +0530

    query params

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,7 +1,13 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET() {
-  return Response.json(comments);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+
+  const filteredComment = query ? comments.filter((comment) => comment.text.toLowerCase().includes(query.toLowerCase())) : comments;
+
+  return Response.json(filteredComment);
 }
 
 export async function POST(request: Request) {
