7be58b02a5f6ecba6638ad76971ec05c3ce43faf
ykji <yvarshney44@gmail.com>
2024-01-18 15:34:31 +0530

    request headers

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,9 +1,16 @@
-import { NextRequest } from "next/server";
 import { comments } from "./data";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const query = searchParams.get("query");
+
+  const requestHeaders = new Headers(request.headers);
+  console.log("new Headers ->", requestHeaders.get("Authorization"));
+
+  const headersList = headers();
+  console.log("next/headers ->", headersList.get("Authorization"));
 
   const filteredComment = query ? comments.filter((comment) => comment.text.toLowerCase().includes(query.toLowerCase())) : comments;
 
