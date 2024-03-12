37898f154edaa021f2988aaf88621e86eaa7ad6f
ykji <yvarshney44@gmail.com>
2024-03-12 17:07:19 +0530

    understand catch all segements in routing

---
+++ src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+// This file will match to every url that containes docs segement.
+
+const Docs = ({
+  params,
+}: {
+  params: {
+    slug: string[];
+  };
+}) => {
+  const slug = params.slug;
+
+  console.log({ slug });
+
+  if (slug?.length === 2) {
+    return (
+      <h1>
+        Docs home page for feature {slug[0]} and concept {slug[1]}
+      </h1>
+    );
+  } else if (slug?.length === 1) {
+    return <h1>Docs home page for feature {slug[0]}</h1>;
+  }
+
+  return <h1>Docs home page</h1>;
+};
+
+export default Docs;
