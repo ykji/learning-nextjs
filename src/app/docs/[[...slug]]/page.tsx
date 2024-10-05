97bfff94fe1b4e0fd8007468d0ce6e42082821c9
ykji <yvarshney44@gmail.com>
2024-10-05 21:48:01 +0530

    not found page 4 ways in total

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { notFound } from 'next/navigation';
+import React from 'react';
 
 // This file will match to every url that containes docs segement.
 
@@ -20,6 +21,10 @@ const Docs = ({
       </h1>
     );
   } else if (slug?.length === 1) {
+    if (slug[0] > '25') {
+      notFound();
+    }
+    
     return <h1>Docs home page for feature {slug[0]}</h1>;
   }
 
---
+++ src/app/docs/not-found.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>Docs not found</div>;
+};
+
+export default NotFound;
